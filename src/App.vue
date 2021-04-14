eeb2b80f98722d626e554481fc8f78e17998c2dc
David Sorrell <davidsoreal33@gmail.com>
2021-04-14 16:18:51 -0500

    create add page with functionality/add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
+  <Navbar />
   <router-view/>
 </template>
+
+<script>
+import Navbar from './components/navbar.vue'
+
+export default {
+  components: { Navbar }
+}
+</script>
 
 <style>
 body {
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import addProject from '../views/addProject'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/add',
+    name: 'addProject',
+    component: addProject
   }
 ]
 
